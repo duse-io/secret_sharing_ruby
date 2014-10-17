8b4ae0041442bd5039e3f6309cd3d9b21d192c2f
flower-pot <flower-pot@flowerpot.io>
2014-10-17 16:34:59 +0200

    add start of documentation

--- lib/secret_sharing.rb
+++ lib/secret_sharing.rb
@@ -6,6 +6,20 @@ require "secret_sharing/charset"
 require "secret_sharing/share"
 
 module SecretSharing
+
+  # Public: Split a secret using Shamir's Secret Sharing algorithm.
+  #
+  # secret_string   - The secret to split.
+  # share_threshold - The number of shares that should be required to recover
+  # the secret.
+  # num_shares      - The total number of shares to generate.
+  #
+  # Example
+  #
+  #   SecretSharing.split_secret("my secret", 2, 3)
+  #   # => ["my secrt-1-0ff4693f", "my secrt-2-2da39008", "my secrt-3-75bd29a"]
+  #
+  # Returns an array of shares that can be used to recover the secret
   def split_secret(secret_string, share_threshold, num_shares)
     charset = Charset.new(secret_string)
     secret_int = charset.s_to_i(secret_string)
@@ -15,6 +29,16 @@ module SecretSharing
     end
   end
 
+  # Public: Recover a secret with an array of string shares.
+  #
+  # raw_shares - The shares to recover the secret in form of an array of strings
+  #
+  # Example
+  #
+  #   SecretSharing.recover_secret(["my secrt-1-0ff4693f", "my secrt-2-2da39008"])
+  #   # => "my secret"
+  #
+  # Returns the recovered secret in a string representation
   def recover_secret(raw_shares)
     shares = raw_shares.map do |raw_share|
       Share.from_string raw_share
