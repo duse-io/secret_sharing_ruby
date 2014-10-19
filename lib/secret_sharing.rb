46f6dfe68a378140f8a3eb233627be530ce3b77d
flower-pot <flower-pot@flowerpot.io>
2014-10-19 21:14:09 +0200

    more documentation

--- lib/secret_sharing.rb
+++ lib/secret_sharing.rb
@@ -5,20 +5,22 @@ require 'secret_sharing/prime'
 require 'secret_sharing/charset'
 require 'secret_sharing/share'
 
+# A ruby implementation of Shamir's Secret Sharing. This module is the only
+# connection to be consumed by a user. Do not use any other class/module of
+# this library unless you really know what you are doing.
 module SecretSharing
-  # Public: Split a secret using Shamir's Secret Sharing algorithm.
-  #
-  # secret_string   - The secret to split.
-  # share_threshold - The number of shares that should be required to recover
-  # the secret.
-  # num_shares      - The total number of shares to generate.
+  # Split a secret using Shamir's Secret Sharing algorithm.
   #
   # Example
   #
   #   SecretSharing.split_secret('secret', 2, 3)
   #   # => ["tcesr-1-4e16", "tcesr-2-1105", "tcesr-3-1d3f3"]
   #
-  # Returns an array of shares that can be used to recover the secret
+  # @param secret_string [String] Secret to split.
+  # @param share_threshold [Integer] Number of shares to recover the secret.
+  # @param num_shares [Integer] Total number of shares to generate.
+  #
+  # @return [Array] Array of shares that can be used to recover the secret.
   def split_secret(secret_string, share_threshold, num_shares)
     charset = Charset.from_string secret_string
     secret_int = charset.s_to_i(secret_string)
@@ -30,16 +32,17 @@ module SecretSharing
     end
   end
 
-  # Public: Recover a secret with an array of string shares.
-  #
-  # raw_shares - The shares to recover the secret in form of an array of strings
+  # Recover a secret with an array of string shares generated with
+  # {#split_secret}.
   #
   # Example
   #
   #   SecretSharing.recover_secret(["tcesr-1-4e16", "tcesr-2-1105"])
   #   # => "secret"
   #
-  # Returns the recovered secret in a string representation
+  # @param raw_shares [String] Shares to recover the secret in form of an array of strings
+  #
+  # @return [String] Recovered secret in a string representation.
   def recover_secret(raw_shares)
     shares = raw_shares.map do |raw_share|
       Share.from_string raw_share
--- lib/secret_sharing/point.rb
+++ lib/secret_sharing/point.rb
@@ -1,7 +1,17 @@
 module SecretSharing
+  # This class is used to store a two dimensional point consisting of integers.
   class Point
     attr_accessor :x, :y
 
+    # Create a Point object.
+    #
+    # Examples
+    #
+    #   Point.new(1, 2)
+    #   # => #<Point: @x=1 @y=2>
+    #
+    # @param x [Integer] The x value of the point
+    # @param y [Integer] The y value of the point
     def initialize(x, y)
       unless x.is_a?(Integer) && y.is_a?(Integer)
         fail ArgumentError, 'Coordinates must be integers'
@@ -11,10 +21,29 @@ module SecretSharing
       @y = y
     end
 
+    # A nicer inspection of a point object than by default.
+    #
+    # @return [String] String representation of a Point
+    #
+    # Examples
+    #
+    #   SecretSharing::Point.new(1, 2).inspect
+    #   # => "#<Point: @x=1 @y=2>"
     def inspect
       "#<Point: @x=#{x} @y=#{y}>"
     end
 
+    # An implementation similar to Array#transpose for Arrays of Points
+    #
+    # @return [Array] Two Arrays in an Array
+    #
+    # Examples
+    #
+    #   point1 = SecretSharing::Point.new(1, 2)
+    #   point2 = SecretSharing::Point.new(3, 4)
+    #   points = [point1, point2]
+    #   Point.transpose(points)
+    #   # => [[1, 3], [2, 4]]
     def self.transpose(points)
       x_values = []
       y_values = []
