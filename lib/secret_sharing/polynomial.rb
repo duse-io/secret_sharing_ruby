2a54fdf4c6bd839b97c4c5a5dabf2905f61486b4
flower-pot <flower-pot@flowerpot.io>
2014-10-24 22:13:05 +0200

    more documentation on polynomial

--- lib/secret_sharing/polynomial.rb
+++ lib/secret_sharing/polynomial.rb
@@ -2,10 +2,30 @@ module SecretSharing
   # The polynomial is used to represent the required random polynomials used in
   # Shamir's Secret Sharing algorithm.
   class Polynomial
+    # Create a new instance of a Polynomial with n coefficients, when having
+    # the polynomial in standard polynomial form.
+    #
+    # Example
+    #
+    #   Polynomial.new [1, 2, 3]
+    #   # => #<SecretSharing::Polynomial:0x0000000 @coefficients=[1, 2, 3]>
+    #
+    # @param coefficients [Array] an array of integers as the coefficients
     def initialize(coefficients)
       @coefficients = coefficients
     end
 
+    # Generate points on the polynomial, that can be used to reconstruct the
+    # polynomial with.
+    #
+    # Example
+    #
+    #   SecretSharing::Polynomial.new([1, 2, 3, 4]).points(3, 7)
+    #   # => [#<Point: @x=1 @y=3>, #<Point: @x=2 @y=0>, #<Point: @x=3 @y=2>]
+    #
+    # @param num_points [Integer] number of points to generate
+    # @param prime [Integer] prime for calculation in finite field
+    # @return [Array] array of calculated points
     def points(num_points, prime)
       (1..num_points).map do |x|
         y = @coefficients[0]
@@ -18,6 +38,17 @@ module SecretSharing
       end
     end
 
+    # Generate a random polynomial with a specific degree, defined x=0 value
+    # and an upper limit for the coefficients of the polynomial.
+    #
+    # Example
+    #
+    #   Polynomial.random(2, 3, 7)
+    #   # => #<SecretSharing::Polynomial:0x0000000 @coefficients=[3, 0, 4]>
+    #
+    # @param degree [Integer] degree of the polynomial to generate
+    # @param intercept [Integer] the y value for x=0
+    # @param upper_bound [Integer] the highest value of a single coefficient
     def self.random(degree, intercept, upper_bound)
       fail ArgumentError, 'Degree must be a non-negative number' if degree < 0
 
@@ -27,6 +58,17 @@ module SecretSharing
       Polynomial.new coefficients
     end
 
+    # Generate points from a secret integer.
+    #
+    # Example
+    #
+    #   SecretSharing::Polynomial.points_from_secret(123, 2, 3)
+    #   # => [#<Point: @x=1 @y=109>, #<Point: @x=2 @y=95>, #<Point: @x=3 @y=81>]
+    #
+    # @param secret_int [Integer] the secret to divide into points
+    # @param point_threshold [Integer] number of points to reconstruct
+    # @param num_points [Integer] number of points to generate
+    # @return [Polynomial] the generated polynomial
     def self.points_from_secret(secret_int, point_threshold, num_points)
       prime = SecretSharing::Prime.large_enough_prime([secret_int, num_points])
       fail ArgumentError, 'Secret is too long' if prime.nil?
@@ -39,6 +81,7 @@ module SecretSharing
       polynomial.points(num_points, prime)
     end
 
+    # Modular lagrange interpolation
     def self.modular_lagrange_interpolation(points)
       y_values = Point.transpose(points)[1]
       prime = SecretSharing::Prime.large_enough_prime(y_values)
@@ -49,6 +92,7 @@ module SecretSharing
       end
     end
 
+    # part of the lagrange interpolation
     def self.lagrange_fraction(points, current, prime)
       numerator, denominator = 1, 1
       points.each do |point|
@@ -60,13 +104,15 @@ module SecretSharing
       [numerator, denominator]
     end
 
+    # inverse modulo
     def self.mod_inverse(k, prime)
       k = k % prime
       r = egcd(prime, k.abs)[2]
       (prime + r) % prime
     end
 
-    def self.egcd(a, b) # extended Euclidean algorithm
+    # extended Euclidean algorithm
+    def self.egcd(a, b)
       return [b, 0, 1] if a == 0
       g, y, x = egcd(b % a, a)
       [g, x - b.div(a) * y, y]
