f47d222fa187fb6def5e1a939e2cf00684bd548e
flower-pot <flower-pot@flowerpot.io>
2014-10-26 10:13:31 +0100

    replace lambda definition with new (->) literal

--- spec/polynomial_spec.rb
+++ spec/polynomial_spec.rb
@@ -1,28 +1,22 @@
 describe SecretSharing::Charset do
   it 'should error when trying to generate a polynomial with negative degree' do
-    random_generator = lambda { SecretSharing::Polynomial.random -1, 1, 12 }
+    random_generator = -> { SecretSharing::Polynomial.random(-1, 1, 12) }
     expect(&random_generator).to raise_error(ArgumentError)
   end
 
   it 'should error when the secret is to long' do
-    secret = 2**10000
-    block = lambda do
-      SecretSharing::Polynomial.points_from_secret(secret, 2, 3)
-    end
+    secret = 2**10_000
+    block = -> { SecretSharing::Polynomial.points_from_secret(secret, 2, 3) }
     expect(&block).to raise_error(ArgumentError)
   end
 
   it 'should error when the point threshold is too small' do
-    block = lambda do
-      SecretSharing::Polynomial.points_from_secret(123, 1, 3)
-    end
+    block = -> { SecretSharing::Polynomial.points_from_secret(123, 1, 3) }
     expect(&block).to raise_error(ArgumentError)
   end
 
   it 'should error when the point threshold is larger than total points' do
-    block = lambda do
-      SecretSharing::Polynomial.points_from_secret(123, 3, 2)
-    end
+    block = -> { SecretSharing::Polynomial.points_from_secret(123, 3, 2) }
     expect(&block).to raise_error(ArgumentError)
   end
 end
